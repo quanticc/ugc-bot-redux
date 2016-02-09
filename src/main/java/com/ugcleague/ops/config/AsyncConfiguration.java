4540eb4c5f9b856a25e5bb6c706ccce5d6001d62
Ivan Abarca <ivan.abarca@usach.cl>
2016-02-08 21:43:19 -0300

    Modifies uncaught exception handler

--- src/main/java/com/ugcleague/ops/config/AsyncConfiguration.java
+++ src/main/java/com/ugcleague/ops/config/AsyncConfiguration.java
@@ -4,7 +4,6 @@ import com.ugcleague.ops.async.ExceptionHandlingAsyncTaskExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -41,6 +41,15 @@ public class AsyncConfiguration implements AsyncConfigurer {
 
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new SimpleAsyncUncaughtExceptionHandler();
+        return new SimpleUncaughtExceptionHandler();
+    }
+
+    private class SimpleUncaughtExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+        @Override
+        public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+            log.error(String.format("Unexpected error occurred invoking async " +
+                "method '%s'.", method), ex);
+        }
     }
 }
