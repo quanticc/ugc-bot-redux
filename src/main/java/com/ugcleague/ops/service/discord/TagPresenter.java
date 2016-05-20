701324d13434c0cde70ada2b6b1cb1f6e9a0529b
Ivan Abarca <iabarca@users.noreply.github.com>
2016-05-20 14:06:28 -0400

    Unrestricted .tag

--- src/main/java/com/ugcleague/ops/service/discord/TagPresenter.java
+++ src/main/java/com/ugcleague/ops/service/discord/TagPresenter.java
@@ -104,7 +104,7 @@ public class TagPresenter {
         aliases.put("delete", "-r");
         aliases.put("info", "-i");
         commandService.register(CommandBuilder.anyMatch(".tag").description("Perform operations to tag and display messages")
-            .support().originReplies().parser(parser).optionAliases(aliases).command(this::executeTag)
+            .unrestricted().originReplies().parser(parser).optionAliases(aliases).command(this::executeTag)
             .limit(3).build());
     }
 
